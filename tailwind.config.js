5fec01dd267a0637c8d114d0b456cf1f22a6bb97
Vivek <vivekpal2618@gmail.com>
2025-01-17 12:55:16 +0530

    initail commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit", // Add this line
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Adjust the path as needed
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
